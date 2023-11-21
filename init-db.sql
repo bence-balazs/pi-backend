df53583c38985a1b417d639463572c9c23175600
bence-balazs <bestbence@gmail.com>
2023-11-21 14:23:27 +0100

    docs: update sql

--- init-db.sql
+++ init-db.sql
@@ -2,7 +2,7 @@
 ALTER DATABASE rpibp
 SET TIMEZONE TO 'Europe/Budapest';
 
-/* create cpu_temp table */
+/* create cpu_temp, uptime, dht22_temp, dht22_hum table */
 CREATE TABLE cpu_temp (
   id serial PRIMARY KEY,
   ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -10,7 +10,7 @@ CREATE TABLE cpu_temp (
 );
 
 /* insert test data to cpu_temp table */
-INSERT INTO cpu_temp (temp) VALUES (33.5);
+INSERT INTO cpu_temp (value) VALUES (33.5);
 
 /* delete all record from cpu_temp table */
 DELETE FROM cpu_temp WHERE id < 999;
